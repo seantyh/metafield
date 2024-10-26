d12959bd65764692c5408bb47fb88c72db9b46a9
seantyh <seantyh@gmail.com>
2024-10-26 16:30:25 +0200

    update nb_depends

--- src/metafield/__init__.py
+++ src/metafield/__init__.py
@@ -1,3 +1,7 @@
 from .nb_utils import parse_hash, find_tagged_cell, find_section
 from .git_utils import find_friends
-from .nb_depends import build_dependency_graph, build_subgraph, visualize
+from .nb_depends import (build_full_graph, 
+                         build_subgraph, 
+                         search_node,
+                         visualize, 
+                         data_deps)
--- src/metafield/nb_depends.py
+++ src/metafield/nb_depends.py
@@ -6,9 +6,24 @@ import networkx as nx
 
 from .nb_utils import parse_hash, find_tagged_cell, find_section
 
-def build_dependency_graph(repo_dir):
+def search_node(G, node_query, first_only=True):
+  nodes = []
+  for node in G.nodes:
+    if node_query.lower() in node.lower():
+      nodes.append(node)
+  
+  if nodes and first_only:
+    return nodes[0]
+  else:
+    return nodes
+
+def build_full_graph(repo_dir=None):
+  if repo_dir is None:
+    import subprocess
+    repo_dir = subprocess.check_output(["git", "rev-parse", "--show-toplevel"])
+    repo_dir = repo_dir.decode("utf-8").strip()
   nb_paths = Path(repo_dir).rglob("*.ipynb")  
-  G = nx.DiGraph()
+  G = nx.DiGraph(repo_dir=repo_dir)
 
 
   for nb_path in nb_paths:      
@@ -37,29 +52,32 @@ def build_dependency_graph(repo_dir):
     output_hashes = list(chain.from_iterable(map(parse_hash, output_cells)))
 
     for input_hash, input_path in input_hashes:
-      if not G.has_node(input_hash):
-        ihash = input_hash[:6]
-        G.add_node(ihash, file_path=input_path, node_type="data")
-        G.add_edge(ihash, nb_label)
+      ihash = input_hash[:7]
+      if not G.has_node(ihash):
+        G.add_node(ihash, file_path=input_path, node_type="data", sha1=input_hash)
+      G.add_edge(ihash, nb_label)
     
     for output_hash, output_path in output_hashes:
-      if not G.has_node(output_hash):
-        ohash = output_hash[:6]
-        G.add_node(ohash, file_path=output_path, node_type="data")
-        G.add_edge(nb_label, ohash)
+      ohash = output_hash[:7]
+      if not G.has_node(ohash):
+        G.add_node(ohash, file_path=output_path, node_type="data", sha1=output_hash)
+      G.add_edge(nb_label, ohash)
     
   return G
 
-def build_subgraph(G, target: str):
+def build_subgraph(G, target: str, depth=None):
   tgt_node = None
   for node_x in G.nodes:
     if target in node_x:
       tgt_node = node_x
       break  
   
-  rev_edges = nx.edge_bfs(G, tgt_node, orientation="reverse")
-  fwd_edges = nx.edge_bfs(G, tgt_node, orientation="original")
-  visited_edges = list(rev_edges) + list(fwd_edges)
+  if not tgt_node:
+    raise ValueError(f"Node {target} not found in graph")
+  
+  rev_edges = nx.bfs_edges(G, tgt_node, reverse=True, depth_limit=depth)
+  fwd_edges = nx.bfs_edges(G, tgt_node, reverse=False, depth_limit=depth)
+  visited_edges = [(x[1], x[0]) for x in rev_edges] + list(fwd_edges)  
   sG = G.edge_subgraph([(x[0], x[1]) for x in visited_edges])
   return sG
 
@@ -88,3 +106,8 @@ def visualize(G, hide_label=True):
   fd, path = tempfile.mkstemp(suffix=".png")
   pydot_G.write_png(path)  # type: ignore
   return path
+
+def data_deps(G, target: str, depth=None):
+  sG = build_subgraph(G, target, depth)
+  vpath = visualize(sG, hide_label=False)
+  return vpath, sG
